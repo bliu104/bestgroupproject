252731b0308b6c19d9e0878313198f6ab1be797d
kathleenfwang <kat@Kathleens-MacBook-Air.local>
2020-03-11 13:49:58 -0400

    add darkmode

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -4,44 +4,64 @@ import Routes from '../routes'
 import Header from '../screens/Header'
 
 export default class Container extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: null,
-            items: []
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: null,
+      items: [],
+      isLight: true
     }
+  }
 
-    async componentDidMount() {
-        try {
-            const items = await getItems()
-            this.setState({ items })
-        } catch (err) {
-            console.error(err)
-        }
+  async componentDidMount() {
+    try {
+      const items = await getItems()
+      this.setState({ items })
+    } catch (err) {
+      console.error(err)
     }
+  }
 
-    addItem = item => this.setState({ items: [...this.state.items, item] })
+  addItem = item => this.setState({ items: [...this.state.items, item] })
 
-    setUser = user => this.setState({ user })
+  setUser = user => this.setState({ user })
 
-    clearUser = () => this.setState({ user: null })
+  clearUser = () => this.setState({ user: null })
 
-    render() {
-        const { user, items } = this.state
-        return (
-            <>
-                <Header user={user} />
-                <main className="container">
-                    <Routes
-                        items={items}
-                        user={user}
-                        setUser={this.setUser}
-                        addItem={this.addItem}
-                        clearUser={this.clearUser}
-                    />
-                </main>
-            </>
-        )
+  handleMode = (e) => {
+    console.log('clicked')
+    let { isLight } = this.state 
+   
+    if (isLight) {
+      this.setState({
+        isLight: false
+      })
+      e.target.parentElement.parentElement.className = "dark"
     }
+    else {
+      this.setState({ isLight: true })
+      e.target.parentElement.parentElement.className = "light"
+    }
+  }
+
+  render() {
+
+    const { user, items, isLight } = this.state
+    const { handleMode } = this
+    return (
+      <>
+        <Header user={user} />
+        <button onClick={handleMode} id ="toggleButton">{isLight ? "Dark" : "Light"} Mode</button>
+        <main className="container">
+          <Routes
+            items={items}
+            user={user}
+            setUser={this.setUser}
+            addItem={this.addItem}
+            clearUser={this.clearUser}
+          />
+        </main>
+      </>
+    )
+  }
 }
