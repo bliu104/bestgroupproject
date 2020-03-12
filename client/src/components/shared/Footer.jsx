753b321dba51dbb65b0cc03a4619b31f72f9ca24
Your Name <you@example.com>
2020-03-12 09:06:06 -0400

    added contact us for footer

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+// import ' /Footer.css'
+
+const mailChimp = `
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://gmail.us19.list-manage.com/subscribe/post?u=22e747e9e145011027f6605b2&amp;id=759f502c55" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Subscribe</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_22e747e9e145011027f6605b2_759f502c55" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>`
 
 const Footer = () => (
   <footer>
+    
     <p>© Copyright {new Date().getFullYear()}. All Rights Reserved.</p>
+    
   </footer>
 )
 
